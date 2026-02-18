26b7f7db68199c3d4bedddc0fb45264203389e77
Evening Swan <rajaadarsh25@gmail.com>
2026-02-18 23:23:31 +0530

    added getProjectsByWorkspace(day9)

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -1,11 +1,13 @@
+const projects = require("../models/projects");
 const Projects = require("../models/projects");
+const workspace = require("../models/workspace");
 const Workspace = require("../models/workspace");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 async function createProject(req, res) {
   try {
-      const workspaceId = req.params.id;
-      const { projectName } = req.body;
+    const workspaceId = req.params.id;
+    const { projectName } = req.body;
     if (!mongoose.Types.ObjectId.isValid(workspaceId)) {
       return res.status(400).json({ message: "Invalid workspace id" });
     }
@@ -51,6 +53,42 @@ async function createProject(req, res) {
   }
 }
 
+async function getProjectsByWorkspace(req, res) {
+  try {
+    const workspaceId = req.params.id;
+    const workspaceById = await workspace.findById(workspaceId);
+    if (!workspaceById) {
+      return res.status(404).json({
+        message: "Workspace Not Found",
+      });
+    }
+    const isMember = workspaceById.members.some(
+      (memberId) => memberId.toString() === req.user._id.toString(),
+    );
+    if (!isMember) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    const projectsByWorkspaceId = await projects.find({
+      workspace: workspaceId,
+    });
+    const result = projectsByWorkspaceId.map((ws) => ({
+      id: ws._id,
+      name: ws.name,
+      workspace: ws.workspace,
+      createdBy: ws.createdBy,
+    }));
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+}
+
 module.exports = {
   createProject,
+  getProjectsByWorkspace,
 };
--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express();
 const authMiddleware = require('../middlewares/authmiddleware');
-const { createProject } = require("../controllers/projectController");
+const { createProject, getProjectsByWorkspace } = require("../controllers/projectController");
 
 
 router.post('/workspaces/:id/projects', authMiddleware, createProject)
+router.get('/workspaces/:id/projects', authMiddleware, getProjectsByWorkspace)
 
 module.exports = router;
