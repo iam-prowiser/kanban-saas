914204c98da8dd16612c0455b32ff01db3cec2a0
Evening Swan <rajaadarsh25@gmail.com>
2026-02-21 10:26:21 +0530

    added updateTaskStatus(day12)

--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/authmiddleware')
-const { createTask, getTasksByProject } = require('../controllers/taskController')
+const { createTask, getTasksByProject, updateTaskStatus } = require('../controllers/taskController')
 const router = express.Router()
 
 router.post('/projects/:id/tasks', authMiddleware, createTask)
 router.get("/projects/:id/tasks", authMiddleware, getTasksByProject);
+router.patch("/tasks/:id", authMiddleware, updateTaskStatus);
 
 module.exports = router;
