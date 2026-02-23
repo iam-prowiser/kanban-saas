7c068bd40b50c82e07aa8fbcc17c3a5b3e8bd063
Evening Swan <rajaadarsh25@gmail.com>
2026-02-23 16:10:38 +0530

    added Backend Completed\(day14)

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -226,4 +226,51 @@ async function updateTaskDetails(req, res) {
     });
   }
 }
-module.exports = { createTask, getTasksByProject, updateTaskStatus, updateTaskDetails };
+
+async function deleteTask(req, res) {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({
+        message: "Task Not found",
+      });
+    }
+    const projectExists = await projects.findById(task.project);
+    if (!projectExists) {
+      return res.status(404).json({
+        message: "Project not found",
+      });
+    }
+    const workspaceExists = await workspace.findById(projectExists.workspace);
+    if (!workspaceExists) {
+      return res.status(404).json({
+        message: "Workspace not found",
+      });
+    }
+    const isMember = workspaceExists.members.some(
+      (memberId) => memberId.toString() === req.user._id.toString(),
+    );
+    if (!isMember) {
+      return res.status(403).json({
+        message: "Not Authorized",
+      });
+    }
+    await task.deleteOne();
+    return res.status(200).json({
+      message: "Task Successfully Deleted",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+}
+module.exports = {
+  createTask,
+  getTasksByProject,
+  updateTaskStatus,
+  updateTaskDetails,
+  deleteTask,
+};
--- server/src/routes/taskRoutes.js
+++ server/src/routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/authmiddleware')
-const { createTask, getTasksByProject, updateTaskStatus, updateTaskDetails } = require('../controllers/taskController')
+const { createTask, getTasksByProject, updateTaskStatus, updateTaskDetails, deleteTask } = require('../controllers/taskController')
 const router = express.Router()
 
 router.post('/projects/:id/tasks', authMiddleware, createTask)
 router.get("/projects/:id/tasks", authMiddleware, getTasksByProject);
 router.patch("/tasks/:id", authMiddleware, updateTaskStatus);
 router.patch("/tasks/:id", authMiddleware, updateTaskDetails);
+router.delete("/tasks/:id", authMiddleware, deleteTask);
 
 module.exports = router;
